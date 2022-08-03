ba6e414b5685e4deafec730fbd1dd4bee8e68cb7
Hymeng <102722435+HymengZ@users.noreply.github.com>
2022-08-03 15:28:27 +0800

    Add files via upload

---
+++ python01/Python_1.py
@@ -0,0 +1,17 @@
+# 欢迎语HelloPython第一个注释
+print("你好")
+
+# 欢迎语HelloPython第一个注释
+print("Hello Python")  # 输出你好
+
+"""
+这是一个多行注释
+这是多行注释
+多行注释
+
+"""
+print("Hello Python")  # 输出你好Python
+print(10/20)
+
+print(9//2)
+print("-" * 80)
